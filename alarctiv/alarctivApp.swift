90c4870f66a66c9e52c792eaaac3f87ff5f49303
Okikiola <okikday@gmail.com>
2026-09-13 09:17:54 +0100

    Initial Commit

---
+++ alarctiv/alarctivApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarctivApp.swift
+//  alarctiv
+//
+//  Created by Okikiola on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct alarctivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
